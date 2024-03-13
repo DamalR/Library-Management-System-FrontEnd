2c129307ae07b2cff0770c26b85f410f9f088a51
Damal Ranula <ranuladamal@gmail.com>
2024-03-13 19:26:17 +0530

    user register data binding.

--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component,OnInit } from '@angular/core';
 import { HttpClient,HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { CommonModule} from "@angular/common";
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -14,6 +15,8 @@ export class RegisterComponent implements OnInit{
   private http;
   public countryList:any;
   public selectedCountry:any;
+  public isExistUser:any;
+  public selectCountryCode:any;
   public userObj={
     firstName : null,
     lastName : null,
@@ -41,7 +44,37 @@ export class RegisterComponent implements OnInit{
   }
   setSelectedCountry(country:any){
     this.selectedCountry=country;
+    this.selectCountryCode=country.idd.root+""+country.idd.suffixes[0];
     console.log(this.selectedCountry);
+    console.log(this.selectCountryCode);
+  }
+
+  submitForm(){
+    console.log(this.userObj);
+    this.http.get(`http://localhost:8080/user/is-exist-user/${this.userObj.userName}`).subscribe(data=>{
+      console.log(data);
+      this.isExistUser=data;
+      this.registerUser(this.isExistUser);
+    })
+  }
+  registerUser(isExistUser:any){
+    if(!isExistUser==true){
+        this.http.post("http://localhost:8080/user/add-user",this.userObj).subscribe(data=>{
+          Swal.fire({
+            title:"Success",
+            text: `${this.userObj.userName} has been registered`,
+            icon: "success"
+        })
+        })
+    }else{
+      Swal.fire({
+          title:"can't register this user.",
+          text: `${this.userObj.userName} has already been registered`,
+          icon: "error"
+      })
+    }
+    console.log(isExistUser);
+
   }
 
 }
